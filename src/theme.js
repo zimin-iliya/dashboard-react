9bad530f4f254494f2ab7eded17316875ac7c457
zimin-iliya <zimin.box@gmail.com>
2023-01-24 13:50:21 +0200

    setup

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+
+//colors
+const primary = "#666666";
+const secondary = "#141b2d";
+const error = "#4cceac";
+const warning = "#db4f4a";
+const info = "#6870fa";
