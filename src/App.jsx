6cb2bfa488c6494d91b36a9254408cdbfe0e8e52
zimin-iliya <zimin.box@gmail.com>
2023-02-06 19:32:34 +0200

    calendar start 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import Team from "./scenes/team";
 import Invoices from "./scenes/invoice";
 import Contacts from "./scenes/constacts";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
+import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geograpy from "./scenes/geograpy";
-// import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = UseMode();
@@ -32,12 +32,12 @@ function App() {
               <Route path="/team" element={<Team/>} />
               <Route path="/contacts" element={<Contacts/>} />
               <Route path="/invoices" element={<Invoices/>} />
-              {/* <Route path="/form" element={<Form/>} /> */}
+              <Route path="/form" element={<Form/>} />
               {/* <Route path="/bar" element={<Bar/>} /> */}
               {/* <Route path="/pie" element={<Pie/>} /> */}
               {/* <Route path="/faq" element={<Line/>} /> */}
               {/* <Route path="/geography" element={<Geography/>} /> */}
-              {/* <Route path="/calendar" element={<Calendar/>} /> */}
+              <Route path="/calendar" element={<Calendar/>} />
 
 
             </Routes>
---
+++ src/scenes/form/index.jsx
@@ -0,0 +1,146 @@
+import { Box, TextField } from "@mui/material";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../componets/Header";
+
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: "",
+};
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+const userSchema = Yup.object().shape({
+  firstName: Yup.string().required("First Name is Required"),
+  lastName: Yup.string().required("Last Name is Required"),
+  email: Yup.string().email("Invalid Email").required("Email is Required"),
+  contact: Yup.string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required(" Required"),
+  address1: Yup.string().required("Address is Required"),
+  address2: Yup.string().required("Address is Required"),
+});
+
+const Form = () => {
+  const isNotMobile = useMediaQuery("(min-width: 600px)");
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+  return (
+    <Box m="20px">
+      <Header title="CREATE" subtitle="Create a New User Profile" />
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={userSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleFormSubmit,
+        }) => (
+          <form onSubmit={handleFormSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4,minmax(0, 1fr ))"
+              sx={{
+                "& > div": { gridColumn: isNotMobile ? undefined : "span 4" },
+              }}
+            >
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="First Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.firstName}
+                name="firstName"
+                error={!!touched.firstName && !!errors.firstName}
+                helperText={touched.firstName && errors.firstName}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Last Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.lastName}
+                name="lastName"
+                error={!!touched.lastName && !!errors.lastName}
+                helperText={touched.lastName && errors.lastName}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={!!touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
+                sx={{ gridColumn: "span 4" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Contact Number"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.contact}
+                name="contact"
+                error={!!touched.contact && !!errors.contact}
+                helperText={touched.contact && errors.contact}
+                sx={{ gridColumn: "span 4" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Address Line 1"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address1}
+                name="address1"
+                error={!!touched.address1 && !!errors.address1}
+                helperText={touched.address1 && errors.address1}
+                sx={{ gridColumn: "span 4" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Address Line 2"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address2}
+                name="address2"
+                error={!!touched.address2 && !!errors.address2}
+                helperText={touched.address2 && errors.address2}
+                sx={{ gridColumn: "span 4" }}
+              />
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
+
+export default Form;
